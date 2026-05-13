59f060f36ea4343d21caa5528a7583404146d813
gynbetfc <gyn.bet.fc@gmail.com>
2026-05-13 12:07:27 -0300

    🧹 Limpeza total

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "EYELIVE - Instalacao"
-pkg install python termux-api -y -qq 2>/dev/null
-pip install flask requests -q 2>/dev/null
-echo "alias eyelive='bash <(curl -4 -s https://raw.githubusercontent.com/gynbetfc/eyelive/main/eyelive.sh)'" >> ~/.bashrc
-source ~/.bashrc 2>/dev/null
-echo "Pronto! Digite: eyelive"
