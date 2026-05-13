87c3cb09ab637ebd05ea0fc610eae0263be20650
gynbetfc <gyn.bet.fc@gmail.com>
2026-05-12 21:34:33 -0300

    Eyelive

---
+++ eyelive.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "EYELIVE"
+rm -f bot.py 2>/dev/null
+pkill -f python 2>/dev/null
+sleep 1
+curl -4 -s "https://raw.githubusercontent.com/gynbetfc/eyelive/main/main.py" -o bot.py
+python bot.py &
+sleep 5
+termux-open-url http://127.0.0.1:5050 2>/dev/null
+echo "http://127.0.0.1:5050"
+wait
+rm -f bot.py
